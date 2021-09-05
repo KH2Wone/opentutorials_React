bebc72c7f006abcf750cfda6a73171aabe425bc6
KangHeeWon <kkhw1202@gmail.com>
2021-09-06 01:31:36 +0900

    Add state content of lists

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 
 class TOC extends Component {
     render() {
+        const data = this.props.data;
+        // let lists = [];
+        // let i=0;
+        // while (i<data.length) {
+        //     lists.push(<li key={data[i].id}><a href={`/content/${data[i].id}`}>{data[i].title}</a></li>)
+        //     i++;
+        // }
+        let lists = data.map(v => {
+            return (
+                <li key={v.id}><a href={`/content/${v.id}`}>{v.title}</a></li>
+            )
+        })
         return (
             <nav>
             <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
+                {lists}
             </ul>
             </nav>
         );
