d678a11ca8ce013d5580055c4b5d90916d32928d
KangHeeWon <kkhw1202@gmail.com>
2021-09-09 23:34:20 +0900

    Test setState, bind

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class Subject extends Component {
-    render() {
-        return (
-        <header>
-            <h1><a href="">{this.props.title}</a></h1>
-            {this.props.sub}
-        </header>
-        );
-    }
-}
+// class Subject extends Component {
+//     render() {
+//         return (
+//         <header>
+//             <h1><a href="/" onClick={function(e) {
+//                 console.log(e);
+//                 e.preventDefault();
+//                 this.state.mode = 'welcome';
+//             }.bind(this)}>{this.props.title}</a></h1>
+//             {this.props.sub}
+//         </header>
+//         );
+//     }
+// }
 
-export default Subject;
+// export default Subject;
