4927f5b094ccc85a7e441844f2dbfe6c284259bf
KangHeeWon <kkhw1202@gmail.com>
2021-09-18 17:03:27 +0900

    Add form in CreateContent

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -5,7 +5,18 @@ class CreateContent extends Component {
         return (
             <article>
                 <h2>Create</h2>
-                <form action=""></form>
+                <form action="/create_process" method="post" onSubmit={
+                    function(e) {
+                        e.preventDefault();
+                        alert('submit');
+                    }
+                }>
+                    <p><input type="text" name="title" placeholder="title" /></p>
+                    <p>
+                        <textarea name="desc" placeholder="description"></textarea>
+                    </p>
+                    <p><input type="submit" /></p>
+                </form>
             </article>
         );
     }
