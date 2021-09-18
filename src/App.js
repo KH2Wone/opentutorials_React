2c641116a09c6d8a6f2f8519a5bfb72d550bce61
KangHeeWon <kkhw1202@gmail.com>
2021-09-18 15:33:27 +0900

    Update Content component to Create&Read component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import TOC from "./components/TOC";
 import Subject from "./components/Subject";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import Control from "./components/Control";
 import './App.css';
 
@@ -21,10 +22,11 @@ class App extends Component {
     }
   }
   render() {
-    let _title, _desc = null;
+    let _title, _desc, _article = null;
     if(this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if(this.state.mode === 'read') {
       var i=0;
       while (i < this.state.contents.length) {
@@ -36,6 +38,9 @@ class App extends Component {
         }
         i++;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>;
     }
     return (
       <div className="App">
@@ -51,7 +56,7 @@ class App extends Component {
             mode: _mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div>
     );
   }
