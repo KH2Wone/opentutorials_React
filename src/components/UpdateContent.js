5b7220236bb60d6aa45187f28280ca1db2ac0d96
KangHeeWon <kkhw1202@gmail.com>
2021-09-24 19:58:11 +0900

    Implement connect value in 'update',refactoring

--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 
 class UpdateContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            title: this.props.data.title,
+            desc: this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
     render() {
-        console.log(this.props.data);
         return (
             <article>
                 <h2>Update</h2>
@@ -15,9 +25,15 @@ class UpdateContent extends Component {
                         );
                     }.bind(this)
                 }>
-                    <p><input type="text" name="title" placeholder="title" /></p>
+                    <p><input 
+                    type="text" 
+                    name="title" 
+                    placeholder="title" 
+                    value={this.state.title} 
+                    onChange={this.inputFormHandler} 
+                    /></p>
                     <p>
-                        <textarea name="desc" placeholder="description"></textarea>
+                        <textarea name="desc" placeholder="description" value={this.state.desc} onChange={this.inputFormHandler}></textarea>
                     </p>
                     <p><input type="submit" /></p>
                 </form>
