ec064423c492ae310daf8848daec8f2f0b8741e1
KangHeeWon <kkhw1202@gmail.com>
2021-09-25 15:50:42 +0900

    Make update, change mode, add id

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       var data = this.state.contents[i];
       if(data.id === this.state.selected_content_id) {
         return data;
-        // break;
+        break;
       }
       i++;
     }
@@ -46,24 +46,34 @@ class App extends Component {
     } else if (this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         this.max_content_id = this.max_content_id+1;
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
+
         this.setState({
-          contents: _contents
+          contents: _contents,
+          mode: 'read',
+          selected_content_id: this.max_content_id
         });
       }.bind(this)}></CreateContent>;
     } else if (this.state.mode === 'update') {
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc) {
-        this.max_content_id = this.max_content_id+1;
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+        var _contents = Array.from(this.state.contents);
+        let i = 0;
+        while(i < _contents.length) {
+          if(_contents[i].id === _id) {
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i++;
+        }
         this.setState({
-          contents: _contents
+          contents: _contents,
+          mode: 'read'
         });
-      }.bind(this)}></UpdateContent>;
+      }.bind(this)
+    }></UpdateContent>;
     }
     return _article;
   }
