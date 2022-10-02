4c1f4823b9ff3313040e0e76a76f9fe9c2806f59
KangHeeWon <kkhw1202@gmail.com>
2022-10-02 20:20:58 +0900

    Add code to show control icon files on the screen

--- src/components/Control.js
+++ src/components/Control.js
@@ -3,19 +3,19 @@ import React, {Component} from 'react';
 class Control extends Component {
     render() {
         return (
-            <ul>
+            <ul className="controls">
                 <li><a href="/create" onClick={function(e) {
                     e.preventDefault();
                     this.props.onChangeMode('create');
-                }.bind(this)}>create</a></li>
+                }.bind(this)}><img src="./img/create.png" alt="create" />create</a></li>
                 <li><a href="/update" onClick={function(e) {
                     e.preventDefault();
                     this.props.onChangeMode('update');
-                }.bind(this)}>update</a></li>
-                <li><input type="button" value="delete" onClick={function(e) {
+                }.bind(this)}><img src="./img/update.png" alt="update" />update</a></li>
+                <li><input type="image" src="./img/delete.png" alt="delete" value="delete" onClick={function(e) {
                     e.preventDefault();
                     this.props.onChangeMode('delete');
-                }.bind(this)}></input></li>
+                }.bind(this)} /></li>
             </ul>
         );
     }
